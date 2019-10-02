c144f026b7e9875a0648a1925b3b1e41de6f9e1a
Sarthak <sgrover@protonmail.ch>
2019-10-03 03:34:55 +0530

    update blacklist csv

--- blacklist_detector.py
+++ blacklist_detector.py
@@ -16,7 +16,7 @@ import csv
 
 BLACKLIST_URLS = [
     ("https://rules.emergingthreats.net/blockrules/compromised-ips.txt", 'ip', 'csv'),
-    ("http://reputation.alienvault.com/reputation.data", 'ip', 'split#0'),
+    #("http://reputation.alienvault.com/reputation.data", 'ip', 'split#0'),
 ]
 
 blacklist_ips_savefile = "blacklist_ips.csv"
@@ -39,8 +39,8 @@ class Blacklist(object):
                 fin = csv.reader(csvfile)
                 self.__blacklist = list(fin)
 
-        print("update blacklist")
-        self.__update()
+        #print("update blacklist")
+        #self.__update()
 
     def __update(self):
         """
